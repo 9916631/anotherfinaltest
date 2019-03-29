0c3496bc48568877fe06e88952d751bb7595db6b
9916631 <ssmyth25@gmail.com>
2019-03-30 10:04:09 +1300

    last test before test

---
+++ last test/last test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace last_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("guess bird, duck chick or sparrow");
+
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "duck":
+                case "chick":
+                    Console.WriteLine("Wrong");
+                    break;
+                default:
+                case "sparrow":
+                    Console.WriteLine("You got it right");
+                    break;
+                  
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
